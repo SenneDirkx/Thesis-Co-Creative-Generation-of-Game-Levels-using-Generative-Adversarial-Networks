8f82c03d533285617d6c253b57c5a527d9899fd9
Senne Dirkx <dirkxsenneinc@gmail.com>
2022-03-17 22:15:32 +0100

    C_matrix implemented

--- ZELDA/interaction/interact.py
+++ ZELDA/interaction/interact.py
@@ -1,5 +1,9 @@
 import torch
-from model_generator import GeneratorConv
+from model_generator import GeneratorConv, gumbel_softmax
+import numpy as np
+from utils import subsequence, make_loader, RunningSecondMoment, call_compute
+import copy
+from torch.utils.data import TensorDataset
 
 nz = 128
 temperature = 1.0
@@ -8,38 +12,76 @@ generator = GeneratorConv(nz, temperature)
 generator.load_state_dict(torch.load("./generator.pth"))
 generator.eval()
 
-activation = {}
-def get_activation(name):
-    def hook(model, input, output):
-        activation[name] = output.detach()
-    return hook
+firstlayer = '3'
+lastlayer = '3'
 
-generator.main[2].register_forward_hook(get_activation('relu1'))
-generator.main[3].register_forward_hook(get_activation('conv_transpose2'))
+model = copy.deepcopy(generator)
+
+context_model = subsequence(
+            model.main, upto_layer=firstlayer,
+            share_weights=True)
+
+target_model = subsequence(
+            model.main,
+            first_layer=firstlayer,
+            last_layer=lastlayer,
+            share_weights=True)
+
+rendering_model = subsequence(
+            model.main, after_layer=lastlayer,
+            share_weights=True)
+
+first_layer = [c for c in generator.modules()
+    if isinstance(c, (torch.nn.Conv2d, torch.nn.ConvTranspose2d,
+                        torch.nn.Linear))][0]
+
+seed = 1
+size = 100
+# 4d input if convolutional, 2d input if first layer is linear.
+if isinstance(first_layer, (torch.nn.Conv2d, torch.nn.ConvTranspose2d)):
+    depth = first_layer.in_channels
+    rng = np.random.RandomState(seed)
+    zds = torch.from_numpy(
+        rng.standard_normal(size * depth * 16)
+        .reshape(size, depth, 4, 4)).float()
+else:
+    depth = first_layer.in_features
+    rng = np.random.RandomState(seed)
+    zds = torch.from_numpy(
+        rng.standard_normal(size * depth)
+        .reshape(size, depth)).float()
+
+zds = TensorDataset(zds)
+
+with torch.no_grad():
+    z_sample = zds[0][0][None]
+    k_sample = context_model(z_sample)
+    v_sample = target_model(k_sample)
+    x_sample = gumbel_softmax(rendering_model(v_sample), temperature)
+
+    def separate_key_reps(zbatch):
+        acts = context_model(zbatch)
+        sep_pix = acts.permute(0, 2, 3, 1).reshape(-1, acts.shape[1])
+        print(sep_pix.shape)
+        return sep_pix
+    
+    loader = make_loader(zds, None, 10)
+    r2mom = RunningSecondMoment()
+    for batch in loader:
+        sample = call_compute(separate_key_reps, batch)
+        r2mom.add(sample)
+    
+    c_matrix = r2mom.moment()
+print('c_matrix', c_matrix.shape)
 
 
-I = torch.randn(10, nz, 4, 4)
-O = generator(I, temperature, False)
-K = torch.flatten(activation['relu1'], start_dim=1).transpose(0, 1)
-V = torch.flatten(activation['conv_transpose2'], start_dim=1).transpose(0, 1)
+z = torch.randn(1, nz, 4, 4)
+k = context_model(z)
+print(k.shape)
+v = target_model(k)
+x = gumbel_softmax(rendering_model(v), temperature)
 
-print("I", I.shape)
-print("K", K.shape)
-print("V", V.shape)
-print("O", O.shape)
-
-C = torch.matmul(K, K.transpose(0, 1))
-
-print("C", C.shape)
-
-#print(C)
-print(torch.diag(C))
-
-i = torch.randn(1, nz, 4, 4)
-o = generator(i, temperature, False)
-k = torch.flatten(activation['relu1'], start_dim=1).transpose(0, 1)
-v = torch.flatten(activation['conv_transpose2'], start_dim=1).transpose(0, 1)
-
-d = torch.matmul(C.inverse(), k)
+d = torch.matmul(c_matrix.inverse(), k)
+print(d.shape)
 #torch.save(result, f'./levels/generated/{i}/tensor.pt')
 

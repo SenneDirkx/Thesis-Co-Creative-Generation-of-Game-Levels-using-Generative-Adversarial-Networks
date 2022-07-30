f519712591b9e74f35a323d5b942bd761c1f0c7c
Senne Dirkx <dirkxsenneinc@gmail.com>
2022-07-30 20:00:41 +0200

    edited scenario cp2

--- ZELDA/gamegan/copypaste/script_copypaste2.sh
+++ ZELDA/gamegan/copypaste/script_copypaste2.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-python interact_copypaste.py ../ZeldaDungeon02_5000_10Z.pth 10 4 11 16 58 2,2,5,8 6 10,2,13,8 46,55,6,63 10,2,13,8,10,2,13,8,10,2,13,8,10,2,13,8 5000 1024 latent_input.txt 3 ./output/rewritten_cp_generator2_normal.pth 1 0.0001
+python interact_copypaste.py ../ZeldaDungeon02_5000_10Z.pth 10 4 11 16 58 2,2,5,8 6 10,2,13,8 47,38,6,63 10,2,13,8,10,2,13,8,10,2,13,8,10,2,13,8 5000 1024 latent_input.txt 3 ./output/rewritten_cp_generator2_normal.pth 1 0.0001
 cd ..
 echo "generating levels..."
 python main.py copypaste/output/rewritten_cp_generator2_normal.pth 10 4 11 16 copypaste/latent_input.txt copypaste/output/rewritten_output2_normal.json
